1ee38893b9cdc0065873ee41f28fad36f355fc8a
RajoraC <chprashantbadshah.com>
2023-11-10 09:05:38 +0530

    Basic thing for variable

---
+++ 01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 123
+let emailId = "Prashant@google.com"
+var accountPass = 1444
+accountCity ="Shikarpur"
+
+let accountState
+
+// accountId = 153
+
+console.log(accountId);
+emailId = "abc@google.com"
+accountPass = 111
+accountCity = "BSR"
+
+console.log([emailId, accountPass, accountCity, accountState]);
